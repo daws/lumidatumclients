5ab4ceb19ded38146e61fe76d5cb000c2272fd49
Mat Lee <ml7@uw.edu>
2016-08-31 16:13:53 -0700

    Adding first draft of client.

--- python/lumidatumclient/classes.py
+++ python/lumidatumclient/classes.py
@@ -1,7 +1,19 @@
+import requests
+
+
 class LumidatumClient(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, authentication_token, model_id=None, host_address='https://www.lumidatum.com'):
+        self.authentication_token = authentication_token
+        self.model_id = model_id
+        self.host_address = host_address
 
-    def predict(self):
-        pass
+    def getRecommendations(self, parameters, model_id=None):
+        selected_model_id = model_if if model_id else self.model_id
+
+        response = requests.post(
+            os.path.join(self.host_address, 'api/predict', selected_model_id),
+            parameters
+        )
+
+        return response.json()

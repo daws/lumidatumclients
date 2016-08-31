873aa181c5c73337552d0bc1a2a45c8f294e447c
Mat Lee <ml7@uw.edu>
2016-08-31 15:53:41 -0700

    Initial package setup.

---
+++ python/lumidatumclient/classes.py
@@ -0,0 +1,7 @@
+class LumidatumClient(object):
+
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
---
+++ python/setup.py
@@ -0,0 +1,18 @@
+import distutils.core
+
+
+distutils.core.setup(
+    name='lumidatumclient',
+    packages=['lumidatumclient'],
+    version='0.1.0',
+    description='A client for the Lumidatum REST API.',
+    author='Mat Lee',
+    author_email='matt@lumidatum.com',
+    url='https://www.lumidatum.com',
+    download_url='',
+    keywords=['REST', 'machine learning'],
+    classifiers=[],
+    install_requires=[
+        'requests',
+    ]
+)

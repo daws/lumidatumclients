a8b606fc5b95bc3728082cee4341ca8075bab965
Mat Lee <ml7@uw.edu>
2016-09-06 18:00:10 -0700

    Fix for os.path.join with model_id, was breaking on non-string model_id values.

--- python/lumidatumclient/classes.py
+++ python/lumidatumclient/classes.py
@@ -16,7 +16,7 @@ class LumidatumClient(object):
 
         Returns a list of id/score pairs in descending order from the highest score.
         """
-        selected_model_id = str(model_if) if model_id else self.model_id
+        selected_model_id = str(model_id) if model_id else self.model_id
         if selected_model_id is None:
             raise ValueError('model_id must be specified either at initialization of LumidatumClient or in client method call.')
 

ea13270dcfa262f06fe573ed96be350d43054807
Mat Lee <ml7@uw.edu>
2016-12-21 15:31:40 -0800

    Updating Client interface for file upload functions/placeholders.

--- java/com/lumidatum/IClient.java
+++ java/com/lumidatum/IClient.java
@@ -1,5 +1,6 @@
 package com.lumidatum;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -9,6 +10,7 @@ import com.lumidatum.client.AuctionPostParameters;
 import com.lumidatum.client.AuctionResponse;
 import com.lumidatum.client.ProductPostParameters;
 import com.lumidatum.client.ProductResponse;
+import com.lumidatum.client.SendFileResponse;
 
 
 public interface IClient {
@@ -17,4 +19,11 @@ public interface IClient {
     public ProductResponse postPredict(long modelId, ProductPostParameters parameters) throws IOException, JsonProcessingException;
     public AuctionResponse postPredict(AuctionPostParameters parameters) throws IOException, JsonProcessingException;
     public AuctionResponse postPredict(long modelId, AuctionPostParameters parameters) throws IOException, JsonProcessingException;
+
+    // public SendFileResponse sendUserProfiles(String inputFilePath) throws IOException, JsonProcessingException;
+    // public SendFileResponse sendUserProfiles(File inputFile) throws IOException, JsonProcessingException;
+    // public SendFileResponse sendItemProfiles(String intpuFilePath) throws IOException, JsonProcessingException;
+    // public SendFileResponse sendItemProfiles(File inputFile) throws IOException, JsonProcessingException;
+    public SendFileResponse sendTransactionsFile(String inputFilePath) throws IOException, JsonProcessingException;
+    public SendFileResponse sendTransactionsFile(File inputFile) throws IOException, JsonProcessingException;
 }

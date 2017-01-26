d7154d8cd1e68258d0a5c8be86e0a51a09502455
Mat Lee <ml7@uw.edu>
2017-01-26 13:00:13 -0800

    Updating version. TODO: update README for new functions.

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ import distutils.core
 distutils.core.setup(
     name='lumidatumclient',
     packages=['lumidatumclient'],
-    version='0.4.9',
+    version='0.5.1',
     description='A client for the Lumidatum REST API.',
     author='Mat Lee',
     author_email='matt@lumidatum.com',

92558bd6045a7d0f5eb8ff7ee73f6b85eb3cbd18
Mat Lee <ml7@uw.edu>
2017-03-20 15:08:20 -0700

    Test file name tweak.

---
+++ ruby/lumidatum_client/tests/test_client_unit_tests.rb
@@ -0,0 +1,52 @@
+require "minitest/autorun"
+require "./lib/lumidatum_client.rb"
+
+# assert_equal <expected>, <actual>
+
+class ClientInit < Minitest::Test
+  def setup
+    @valid_api_token = "Token <API key>"
+    @valid_model_id = 123
+    @custom_host_address = "http://localhost:8000"
+
+    @default_host_address = "https://www.lumidatum.com"
+  end
+
+  # No specified host/default host
+  def test_client_init
+    new_client = LumidatumClient.new(@valid_api_token, @valid_model_id, @custom_host_address)
+
+    assert_instance_of(LumidatumClient, new_client)
+  end
+
+  def test_default_host
+    new_client = LumidatumClient.new(@valid_api_token, @valid_model_id)
+
+    # Default host should be https://www.lumidatum.com
+    assert_equal(@default_host_address, new_client.instance_variable_get(:@host_address))
+  end
+
+  def test_specifying_a_host
+    new_client = LumidatumClient.new(@valid_api_token, @valid_model_id, @custom_host_address)
+
+    assert_equal(@custom_host_address, new_client.instance_variable_get(:@host_address))
+  end
+
+  def test_nil_model_id_error
+    assert_raises ArgumentError do
+      LumidatumClient.new(@valid_api_token, nil)
+    end
+  end
+
+  def test_nil_api_token_error
+    assert_raises ArgumentError do
+      LumidatumClient.new(nil, @valid_model_id)
+    end
+  end
+end
+
+# Get item recs
+
+# Upload files
+
+# Download files
